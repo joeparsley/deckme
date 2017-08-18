8462f23d3202565dcb8cc43ce93fc89d9f9c8832
Joe Parsley <joewparsley@gmail.com>
2017-08-17 19:11:17 -0700

    add user theme feature to app component, still need to add ngIfs to change background color as well, all themes but jeskai are made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,17 @@ export class AppComponent {
   local;
   public isLoggedIn: boolean;
 
+
+  jundTheme: string = "/src/themes/naya-theme/naya-theme.css";
+  bantTheme: string = "/src/themes/bant-theme/bant-theme.css";
+  grixisTheme: string = "/src/themes/grixis-theme/grixis-theme.css";
+  nayaTheme: string = "/src/themes/naya-theme/naya-theme.css";
+  esperTheme: string = "/src/themes/esper-theme/esper-theme.css";
+  marduTheme: string = "/src/themes/mardu-theme/mardu-theme.css";
+  sultaiTheme: string = "/src/themes/sultai-theme/sultai-theme.css";
+  temurTheme: string = "/src/themes/temur-theme/temur-theme.css";
+  abzanTheme: string = "/src/themes/abzan-theme/abzan-theme.css";
+
   constructor(public authService: AuthenticationService, public authGaurd: AuthGaurdService, public router: Router) {
     this.authService.user.subscribe(user => {
       if (user == null) {
@@ -34,6 +45,34 @@ export class AppComponent {
 
   logout() {
     this.authService.logout();
+  }
+
+  getJund() {
+    $('#userTheme').attr('href', this.jundTheme);
+  }
+  getBant() {
+    $('#userTheme').attr('href', this.bantTheme);
+  }
+  getGrixis() {
+    $('#userTheme').attr('href', this.grixisTheme);
+  }
+  getNaya() {
+    $('#userTheme').attr('href', this.nayaTheme);
+  }
+  getEsper() {
+    $('#userTheme').attr('href', this.esperTheme);
+  }
+  getMardu() {
+    $('#userTheme').attr('href', this.marduTheme);
+  }
+  getSultai() {
+    $('#userTheme').attr('href', this.sultaiTheme);
+  }
+  getTemur() {
+    $('#userTheme').attr('href', this.temurTheme);
+  }
+  getAbzan() {
+    $('#userTheme').attr('href', this.abzanTheme);
   }
 
 }
